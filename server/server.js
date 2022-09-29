6e9843a63d4fbdcf0af46eada19f466c5a8ab6e9
jacky <lb4013@126.com>
2022-09-29 12:35:04 +0000

    Update package-lock.json and server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const path = require('path');
 const render = require('./render');
 const {JS_BUNDLE_DELAY} = require('./delays');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4444;
 const app = express();
 
 app.use((req, res, next) => {
